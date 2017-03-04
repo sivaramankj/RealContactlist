b3db74f25762a2cb070f72583a1681feeb444a2e
sivaraman rajan <sivaramankj@gmail.com>
2017-03-04 13:12:38 +0530

    deleted

--- test project depolouu/Knila Contact List/Data/Domain/ContactInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data.Domain
-{
-    public class ContactInfo
-    {
-        public int ContactId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
